19294b0b1d197c9ef4e220beca2c33f9eaebaef7
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-05 08:57:37 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 53
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- csharp/tests/SubmarineTcrKata.Domain.Tests/SubmarineShould.cs
+++ csharp/tests/SubmarineTcrKata.Domain.Tests/SubmarineShould.cs
@@ -4,57 +4,6 @@ using static SubmarineTcrKata.Domain.Tests.SubmarineTestBuilder;
 
 namespace SubmarineTcrKata.Domain.Tests;
 
-public static class SubmarineVerificationExtensions
-{
-    public static void StatusShouldBe(this ISubmarine submarine, int position, int aim, int depth)
-    {
-        submarine.Position.Should().Be(position);
-        submarine.Aim.Should().Be(aim);
-        submarine.Depth.Should().Be(depth);
-    }
-}
-
-public class SubmarineTestBuilder
-{
-    private int _depth;
-    private int _position;
-    private int _aim;
-
-    private SubmarineTestBuilder()
-    {
-        _aim = 0;
-        _position = 0;
-        _depth = 0;
-    }
-
-    public static SubmarineTestBuilder Submarine()
-        => new();
-
-    public Submarine Build()
-        => new(_depth, _position, _aim);
-
-    public SubmarineTestBuilder WithAim(int aim)
-    {
-        _aim = aim;
-
-        return this;
-    }
-
-    public SubmarineTestBuilder WithPosition(int position)
-    {
-        _position = position;
-
-        return this;
-    }
-
-    public SubmarineTestBuilder WithDepth(int depth)
-    {
-        _depth = depth;
-
-        return this;
-    }
-}
-
 public class SubmarineShould
 {
     [Fact]
@@ -75,7 +24,7 @@ public class SubmarineShould
 
         submarine.ExecuteCommand(command);
 
-        submarine.StatusShouldBe(expectedPosition, 0, 0);
+        submarine.Should().BeEquivalentTo(Submarine().WithPosition(expectedPosition).Build());
     }
 
 
---
+++ csharp/tests/SubmarineTcrKata.Domain.Tests/SubmarineTestBuilder.cs
@@ -0,0 +1,42 @@
+﻿namespace SubmarineTcrKata.Domain.Tests;
+
+public class SubmarineTestBuilder
+{
+    private int _depth;
+    private int _position;
+    private int _aim;
+
+    private SubmarineTestBuilder()
+    {
+        _aim = 0;
+        _position = 0;
+        _depth = 0;
+    }
+
+    public static SubmarineTestBuilder Submarine()
+        => new();
+
+    public Submarine Build()
+        => new(_depth, _position, _aim);
+
+    public SubmarineTestBuilder WithAim(int aim)
+    {
+        _aim = aim;
+
+        return this;
+    }
+
+    public SubmarineTestBuilder WithPosition(int position)
+    {
+        _position = position;
+
+        return this;
+    }
+
+    public SubmarineTestBuilder WithDepth(int depth)
+    {
+        _depth = depth;
+
+        return this;
+    }
+}
