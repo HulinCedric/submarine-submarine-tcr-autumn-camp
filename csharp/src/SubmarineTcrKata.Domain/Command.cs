6ddb981d2324ca706c0fa1c8b51d831e61f5fec0
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-05 09:11:47 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 63
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

---
+++ csharp/src/SubmarineTcrKata.Domain/Command.cs
@@ -0,0 +1,29 @@
+﻿namespace SubmarineTcrKata.Domain;
+
+public record Command(string Name, int Value)
+{
+    public static Command New(string commandDescription)
+    {
+        var commandParts = commandDescription.Split(" ");
+        var commandName = commandParts[0];
+        var commandValue = int.Parse(commandParts[1]);
+        return new Command(commandName, commandValue);
+    }
+
+    public void ExecuteCommand(Submarine submarine)
+    {
+        switch (Name)
+        {
+            case "forward":
+                submarine.Position += Value;
+                submarine.Depth += submarine.Aim * Value;
+                break;
+            case "down":
+                submarine.Aim += Value;
+                break;
+            case "up":
+                submarine.Aim -= Value;
+                break;
+        }
+    }
+}
--- csharp/src/SubmarineTcrKata.Domain/Submarine.cs
+++ csharp/src/SubmarineTcrKata.Domain/Submarine.cs
@@ -11,37 +11,9 @@ public record Submarine(int Depth, int Position, int Aim) : ISubmarine
         Command.New(commandDescription).ExecuteCommand(this);
     }
 
-    public int Aim { get; set; } = Aim;
+    public int Aim { get; internal set; } = Aim;
 
-    public int Position { get; set; } = Position;
+    public int Position { get; internal set; } = Position;
 
-    public int Depth { get; set; } = Depth;
-}
-
-public record Command(string Name, int Value)
-{
-    public static Command New(string commandDescription)
-    {
-        var commandParts = commandDescription.Split(" ");
-        var commandName = commandParts[0];
-        var commandValue = int.Parse(commandParts[1]);
-        return new Command(commandName, commandValue);
-    }
-
-    public void ExecuteCommand(Submarine submarine)
-    {
-        switch (Name)
-        {
-            case "forward":
-                submarine.Position += Value;
-                submarine.Depth += submarine.Aim * Value;
-                break;
-            case "down":
-                submarine.Aim += Value;
-                break;
-            case "up":
-                submarine.Aim -= Value;
-                break;
-        }
-    }
+    public int Depth { get; internal set; } = Depth;
 }
