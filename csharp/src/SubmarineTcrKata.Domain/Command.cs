0dd6ebe70cf868c8cf37398a675f3b3e5b6e3e3a
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-05 09:25:21 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- csharp/src/SubmarineTcrKata.Domain/Command.cs
+++ csharp/src/SubmarineTcrKata.Domain/Command.cs
@@ -5,8 +5,10 @@ public record Command(string Name, int Value)
     public static Command New(string commandDescription)
     {
         var commandParts = commandDescription.Split(" ");
+        
         var commandName = commandParts[0];
         var commandValue = int.Parse(commandParts[1]);
+        
         return new Command(commandName, commandValue);
     }
 
